7647ec8d1158d95a91cc90cd0a13e655524438b3
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2025-10-24 09:47:17 +0200

    layout fixes

--- components/StaticPages/TwTokens.tsx
+++ components/StaticPages/TwTokens.tsx
@@ -2,7 +2,19 @@ export default function TwTokens() {
   return (
     <div
       className="
-      grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 sm:grid-cols-3 sm:grid-cols-4 sm:grid-cols-1 sm:col-span-1 sm:col-span-2 sm:col-span-3 sm:col-span-4
+      grid grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6
+      grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11 grid-cols-12 
+      col-span-1 col-span-2 col-span-3 col-span-4 col-span-5 col-span-6 col-span-12
+      sm:grid-cols-1 sm:grid-cols-2 sm:grid-cols-3 sm:grid-cols-4 sm:grid-cols-6 sm:grid-cols-12 
+      sm:col-span-1 sm:col-span-2 sm:col-span-3 sm:col-span-4 sm:col-span-6 sm:col-span-12 
+      md:grid-cols-4 md:grid-cols-6 md:grid-cols-3 md:grid-cols-12
+      col-span-full 
+      col-start-2
+      gap-4
+      justify-center
+      w-full max-w-none
+      lg:col-start-2 lg:col-span-10 
+      xl:col-start-3 xl:col-span-8
       gap-6
       "
       style={{ display: 'none' }}
