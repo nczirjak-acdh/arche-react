7e295f3a589408bb9a61bd9e553fd9720300769c
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2025-11-26 13:57:12 +0100

    static page updates

---
+++ app/de/faq-de/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import HtmlFromTemplate from '@/components/StaticPages/HtmlFromTemplate';
+import Cookies from 'js-cookie';
+
+export default function metadata() {
+  const lang = Cookies.get('i18nextLng') || 'de';
+  const router = useRouter();
+
+  useEffect(() => {
+    if (lang === 'en') router.replace('/faq');
+  }, [lang, router]);
+
+  if (lang === 'en') return null; // or a spinner while redirecting
+
+  return (
+    <div className="mb-[100px]">
+      <HtmlFromTemplate
+        locale={lang}
+        name="faq"
+        base="https://raw.githubusercontent.com/nczirjak-acdh/arche-react-static-test/refs/heads/main/arche-react"
+      />
+    </div>
+  );
+}
---
+++ app/faq/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import HtmlFromTemplate from '@/components/StaticPages/HtmlFromTemplate';
+import Cookies from 'js-cookie';
+
+export default function metadata() {
+  const router = useRouter();
+  const lang = Cookies.get('i18nextLng') || 'en';
+
+  useEffect(() => {
+    if (lang === 'de') router.replace('/de/einreichungsprozess');
+  }, [lang, router]);
+
+  if (lang === 'de') return null; // or a spinner while redirecting
+
+  return (
+    <div className="mb-[100px]">
+      <HtmlFromTemplate
+        locale={lang}
+        name="faq"
+        base="https://raw.githubusercontent.com/nczirjak-acdh/arche-react-static-test/refs/heads/main/arche-react"
+      />
+    </div>
+  );
+}
